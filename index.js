583e0e1962a73188d73e9426acc66f38376a46db
Anas Ahmed <anasahmed700@gmail.com>
2024-04-16 11:57:39 +0500

    todo cli commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let flag = true;
+while (flag) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: 'What do you want to add in your todo?'
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: 'Do you want to add more',
+            default: false
+        }
+    ]);
+    todos.push(addTask.todo);
+    flag = addTask.addMore;
+    console.log(todos);
+}
